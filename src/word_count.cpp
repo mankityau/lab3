00eb4bbd63f198a77899c10e6c116fd0058c5d47
Yau Man Kit <Yau Man Kit>
2017-09-28 13:39:51 -0700

    Finished wordcount.

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,13 +1,19 @@
 #include "word_count.h"
 
+bool startNewWord(std::string::const_iterator &it){
+    return (*(it-1) == ' ' && *it != ' ');
+}
 // implementation details
 int word_count(const std::string &line, int start_idx) {
+    if (start_idx >= line.length()) {
+        return 0;
+    }
 
+    int nwords = ((line.at(0) == ' ') ? 0 : 1);
 
-    int nwords = 0;
-    for (int i = start_idx; i < line.length(); ++i) {
-        if (line[i] == ' ') {
-            nwords++;
+    for (std::string::const_iterator it = line.begin() + start_idx + 1; it < line.end(); it++){
+        if (startNewWord(it)){
+            nwords ++;
         }
     }
 
